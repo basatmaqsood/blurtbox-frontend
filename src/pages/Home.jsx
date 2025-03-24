c9bc7096366299694340a4fec47117feddd42c71
basat <itzbasatmaqsood@gmail.com>
2025-03-25 00:27:46 +0500

    Frontend deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -189,13 +189,13 @@ export default function Home() {
           >
             {showAddForm ? "Cancel" : "Share Your Confession"}
           </button>
-          <a
-            href="/about"
+          <Link
+            to="/about"
             className="mb-6 bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-lg font-medium flex items-center transition-colors"
           >
             {" "}
             About us
-          </a>
+          </Link>
         </div>
 
         {/* Add Confession Form */}
